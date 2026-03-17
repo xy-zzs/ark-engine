3a672cd430ac9c8d57f6f958005b93f9f784a2b9
zhouzhensheng <zhouzhensheng@viewshine.cn>
2026-03-17 11:00:24 +0800

    data-stater封装

---
+++ engine-data/data-starter/src/main/java/io/ark/engine/data/stater/properties/ArkDataProperties.java
@@ -0,0 +1,27 @@
+package io.ark.engine.data.stater.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+/**
+ * @Description: ark.data.* 配置属性
+ * @Author: Noah Zhou
+ */
+@Data
+@ConfigurationProperties(prefix = "ark.data")
+public class ArkDataProperties {
+    @NestedConfigurationProperty
+    private OrmProperties jooq = new OrmProperties();
+
+    @NestedConfigurationProperty
+    private OrmProperties jpa = new OrmProperties();
+
+    @Data
+    public static class OrmProperties {
+        /**
+         * 是否启用该 ORM 模块。
+         */
+        private boolean enabled = false;
+    }
+}
